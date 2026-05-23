69ce7d4efca494d029f4d2f7a790fe2ba21e36d4
Zherati <Zherati@DESKTOP-CHP9QEK>
2026-05-23 06:42:41 +0300

    add document Generate QR Test

--- DataAccess/Entities/Document.cs
+++ DataAccess/Entities/Document.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
-
 namespace DataAccess.Entities
 {
     public class Document
---
+++ Testing/DataAccess/Entities/Documents/DocumentTests.cs
@@ -0,0 +1,44 @@
+﻿using DataAccess.Entities;
+using FluentAssertions;
+using QRCoder;
+using Svg;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZXing;
+using ZXing.Windows.Compatibility;
+namespace Testing.DataAccess.Entities.Documents
+{
+    public class DocumentTests
+    {
+
+
+        [Fact]
+        public async Task GenerateQRCode_ShouldGenerateQR()
+        {
+
+            //Arrange
+            var refNum = Guid.NewGuid().ToString();
+            var document = new Document
+            {
+                Id = 1,
+                DocumentName = "Doc Test",
+                RefNumber = refNum
+            };
+
+            //Act
+            document.GenerateQRCode();
+            var svgDocument = SvgDocument.FromSvg<SvgDocument>(document.QR);
+            using var bitmap = svgDocument.Draw(300, 300);
+
+            var reader = new BarcodeReader();
+            var result = reader.Decode(bitmap).Text;
+
+            //Assert 
+            result.Should().Be(refNum);
+        }
+    }
+}
